924233c8fce97569720fca4f17764cecc64503b4
shakti66686 <shaktishekhar66686.ss@gmail.com>
2023-09-06 07:41:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(() => {
+    d = new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotation = 30*htime+mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+    hours.style.transform = `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+let showdate=document.getElementById("btn");
+function date (){
+    
+    let today=new Date();
+    document.write(today);
+}
+showdate.addEventListener("click",date,false);
+
+
